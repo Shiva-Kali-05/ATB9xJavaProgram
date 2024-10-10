2b02696e745e09ec0e37e0f2a1199c0a5303a52a
Shiva-Kali-05 <shivakumarkali05@gmail.com>
2024-10-10 08:47:51 +0530

    New files added

---
+++ src/Oct/ex_04102024/Lab007.java
@@ -0,0 +1,13 @@
+package Oct.ex_04102024;
+
+public class Lab007 {
+    static int age = 3;
+
+    public static void main(String [] args)
+
+    {
+        System.out.println("My name is Nihal");
+        System.out.println("My age is:" +age);
+    }
+
+}
---
+++ src/Oct/ex_04102024/Lab008.java
@@ -0,0 +1,11 @@
+package Oct.ex_04102024;
+
+public class Lab008 {
+    static int a_1=25;
+    static int b_0=50;
+    public static void main(String []args)
+    {
+        int num = a_1 + b_0;
+        System.out.println("Addition of two number is:" +num);
+    }
+}
