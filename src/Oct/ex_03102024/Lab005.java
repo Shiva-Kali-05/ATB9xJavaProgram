e16f9647ac534a927eda6146b51124c6125cac4d
Shiva-Kali-05 <shivakumarkali05@gmail.com>
2024-10-10 08:13:54 +0530

    Latest file added

---
+++ src/Oct/ex_03102024/Lab005.java
@@ -0,0 +1,25 @@
+package Oct.ex_03102024;
+
+public class Lab005 {
+    //public static void main(String[] args)
+    {
+        System.out.println("This is valid main method");
+    }//
+    // Invalid main method
+
+   // private static void main (String[] args)
+    {
+        System.out.println("This is invalid method name");
+    }//
+
+    //static void main(String[] args)
+    {
+        System.out.println("Invalid");
+    }//
+    public static void main (String[] shiva)
+    {
+        System.out.println("in place of args we can give any arguments like shiva, it is valid");
+    }
+
+
+}
